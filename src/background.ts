e3031ef181f3695080c0114c2d70cb88b593332b
LeoMouyna <leo.mouyna@contentsquare.com>
2022-09-20 17:30:07 +0200

    feat: keep emojify through navigation. #9

--- src/background.ts
+++ src/background.ts
@@ -4,6 +4,7 @@ import { LocalStorageService, StorageService } from "./storage";
 const ACTIVE_STATUS_KEY = "active";
 const INACTIVE_ICON = "assets/icon-inactive_16.png";
 const ACTIVE_ICON = "assets/icon-active_16.png";
+const EMOJIFY_TAG_ID = "emojify-active";
 
 function reload() {
   return window.location.reload();
@@ -14,12 +15,17 @@ function updateBody(emojifiedContent: string) {
   if (!body) {
     return;
   }
-  body.innerHTML = emojifiedContent;
+  const emojifyTag = `<span style="display: none" id=${EMOJIFY_TAG_ID}></span>`;
+  body.innerHTML = emojifiedContent + emojifyTag;
 }
 
 function getBody() {
   const body = document.getElementsByTagName("body").item(0);
   return body?.innerHTML;
+}
+
+function hasEmojifyTag() {
+  return document.getElementById(EMOJIFY_TAG_ID) ? true : false;
 }
 
 export class ChromeExtenstion {
@@ -44,12 +50,38 @@ export class ChromeExtenstion {
         await Promise.all([
           this.updateExtensionStatus(previousActiveStatus),
           this.updateExtensionIcon(previousActiveStatus),
-          this.updateWebPageContent(previousActiveStatus, tab),
+          this.updateContentIfNeeded(tab.id ?? -1),
         ]);
       } catch (e) {
         console.error(e);
       }
     });
+
+    chrome.tabs.onActivated.addListener(async (info) => {
+      try {
+        await this.updateContentIfNeeded(info.tabId);
+      } catch (e) {
+        console.error(e);
+      }
+    });
+
+    chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+      try {
+        await this.updateContentIfNeeded(tabId);
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  }
+
+  private async hasEmojifyTag(tabId: number) {
+    return (
+      await chrome.scripting.executeScript({
+        target: { tabId },
+        func: hasEmojifyTag,
+        args: [],
+      })
+    )[0].result;
   }
 
   private async updateExtensionStatus(previousActiveStatus: boolean = true) {
@@ -64,14 +96,17 @@ export class ChromeExtenstion {
     return chrome.action.setIcon({ path: icon });
   }
 
-  private async updateWebPageContent(
-    previousActiveStatus: boolean,
-    tab: chrome.tabs.Tab
-  ) {
-    const active = !previousActiveStatus;
+  private async updateContentIfNeeded(tabId: number) {
+    const emojified = await this.hasEmojifyTag(tabId);
+    const active = (await this.storageService.get(ACTIVE_STATUS_KEY)) ?? false;
+    if ((!active && !emojified) || (active && emojified)) return;
+    return this.updateWebPageContent(active, { id: tabId });
+  }
+
+  private async updateWebPageContent(active: boolean, tab: { id: number }) {
     if (!active) {
       return chrome.scripting.executeScript({
-        target: { tabId: tab.id ? tab.id : -1 },
+        target: { tabId: tab.id },
         func: reload,
         args: [],
       });
